be12ce666a1b746f27ea3137bc7929c5ca248b32
jahan-code <jahanzaib9599@gmail.com>
2024-12-29 04:45:26 +0500

    ready for deployment

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "*", // Use your frontend's deployed URL
+        origin: "https://busy-marlene-codexspaces-79079c7e.koyeb.app", // Use your frontend's deployed URL
         methods: ["GET", "POST"],
     },
 });
